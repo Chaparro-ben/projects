136c34418f65f5c9e4863d98d418191d667f09cd
Ben <benbatres1212@gmail.com>
2024-10-25 12:43:32 -0700

    updated javaScript code

--- movie-project.js
+++ movie-project.js
@@ -1,56 +1,48 @@
 // https://www.omdbapi.com/?s=thor&page=&apikey=4053be5e
 // https://www.omdbapi.com/?i=tt3896198&apikey=4053be5e
-const movieSearchBox = document.getElementById("movie-search-box");
-const searchList = document.getElementById("search-list");
-const movieList = document.getElementById("movie-list");
+const movieList = document.querySelector(".movie-list")
+const movieSearchBox = document.getElementById("movie-search-box")
 const searchResultsList = document.getElementById("search-results-list");
 
 async function main(searchTerm) {
-  const movieURL = `https://www.omdbapi.com/?s=${searchTerm}&page=&apikey=4053be5e`;
-  const movieResponse = await fetch(`${movieURL}`);
-  const movieData = await movieResponse.json();
-  if (movieData.Response == "True") displayMovieList(movieData.Search);
+    const movieURL = `https://www.omdbapi.com/?s=${searchTerm}&page=&apikey=4053be5e`
+    const movieResponse = await fetch(`${movieURL}`)
+    const moviesData = await movieResponse.json()
+    movieList.innerHTML = 
+    moviesData.Search.map(movies => displayMovie(movies)).join("")
+
+}
+
+function displayMovie(movies) {
+    searchResultsList.innerHTML = `<h2 class="search__title">Search Results: for "${movieSearchBox.value}"</h2>`
+    return `
+        <div class="movie">
+          <div class="movie-card">
+              <img src="${movies.Poster}" alt="">
+              <h3 class="movie-title">${movies.Title}</h3>
+              <div class="movie-description">
+                  <p class="movie-year">${movies.Year}</p>
+                  <p class="movie-series">${movies.Type}</p>
+              </div>
+          </div>
+      </div>`
+    
+    
 }
 
 function findMovies() {
-  let searchTerm = movieSearchBox.value.trim();
-  if (searchTerm.length > 0) {
-    movieList.classList.remove("hide-movie-list");
-    main(searchTerm);
-  } else {
-    movieList.classList.add("hide-movie-list");
-  }
+    let searchTerm = movieSearchBox.value.trim();
+    if (searchTerm.length > 0) {
+        movieList.classList.remove("hide-movie-list")
+        main(searchTerm)
+    }
+    else {
+        movieList.classList.add("hide-movie-list")
+    }
 }
 
-function displayMovieList(movies) {
-  searchResultsList.innerHTML = `
-    <h2 class="search__title">
-      Search Results: for "${movieSearchBox.value}"
-    </h2>`;
-  movieList.innerHTML = "";
-  for (let i = 0; i < movies.length; i++) {
-    let movieListTerm = document.createElement("div");
-    movieListTerm.dataset.id = movies[i].imdbID;
-    movieListTerm.classList.add("movie-list");
-    if (movies[i].Poster != "N/A") moviePoster = movies[i].Poster;
-    else moviePoster = "image_not_found.png";
+// function filterMovies(event) {
+//   console.log(event.target.value)
+// }
 
-    movieListTerm.innerHTML = `
-      
-      <div class="movie">
-          <div class="movie-card">
-              <img src="${moviePoster}" alt="">
-              <h3 class="movie-title">${movies[i].Title}</h3>
-              <div class="movie-description">
-                  <p class="movie-year">${movies[i].Year}</p>
-                  <p class="movie-series">${movies[i].Type}</p>
-              </div>
-          </div>
-      </div>`;
-    movieList.appendChild(movieListTerm);
-  }
-}
 
-function filterMovies(event) {
-  displayMovieList(event.target.value);
-}
