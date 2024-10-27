6da08ea01072338bdd3d3e1d870d971172924024
Ben <benbatres1212@gmail.com>
2024-10-26 20:04:15 -0700

    updated all projects

--- movie-project.js
+++ movie-project.js
@@ -3,18 +3,42 @@
 const movieList = document.querySelector(".movie-list")
 const movieSearchBox = document.getElementById("movie-search-box")
 const searchResultsList = document.getElementById("search-results-list");
+const filterSelect = document.getElementById("year-filter")
 
-async function main(searchTerm) {
+async function main(searchTerm, sortOrder = 'latest') {
     const movieURL = `https://www.omdbapi.com/?s=${searchTerm}&page=&apikey=4053be5e`
     const movieResponse = await fetch(`${movieURL}`)
     const moviesData = await movieResponse.json()
-    movieList.innerHTML = 
-    moviesData.Search.map(movies => displayMovie(movies)).join("")
+    
+    if (!moviesData.Search) {
+        movieList.innerHTML = '<p>No results found</p>';
+        return;
+    }
+
+    let sortedMovies = [...moviesData.Search];  // Create a copy of the array
+    
+    if (sortOrder === 'latest') {
+        sortedMovies.sort((a, b) => parseInt(b.Year) - parseInt(a.Year));
+    } else if (sortOrder === 'oldest') {
+        sortedMovies.sort((a, b) => parseInt(a.Year) - parseInt(b.Year));
+    }
+
+    movieList.innerHTML = sortedMovies.map(movies => displayMovie(movies)).join("")
 
 }
 
 function displayMovie(movies) {
-    searchResultsList.innerHTML = `<h2 class="search__title">Search Results: for "${movieSearchBox.value}"</h2>`
+    searchResultsList.innerHTML = `
+    <div class="search__wrapper">
+        <h2 class="search__title">Search Results: for "${movieSearchBox.value}"</h2>
+        <select id="year-filter" onchange="findMovies()" class="custom-select">
+            <option value="" disabled selected>Sort</option>
+            <option value="latest">Latest - Oldest</option>
+            <option value="oldest">Oldest - Latest</option>
+        </select>
+    </div>
+    `
+    filterSelect.innerText = ``
     return `
         <div class="movie">
           <div class="movie-card">
@@ -32,17 +56,18 @@ function displayMovie(movies) {
 
 function findMovies() {
     let searchTerm = movieSearchBox.value.trim();
+    const currentSort = filterSelect.value
     if (searchTerm.length > 0) {
         movieList.classList.remove("hide-movie-list")
-        main(searchTerm)
+        main(searchTerm, currentSort)
     }
     else {
         movieList.classList.add("hide-movie-list")
     }
 }
 
-// function filterMovies(event) {
-//   console.log(event.target.value)
-// }
+function filterMovies(event) {
+  main("toy story", event.target.value)
+}
 
 
